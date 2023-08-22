4a7af654f2d1ea7ffd06d7f13fc43fa3fe602f53
Mengwa Ugo <mengwa44@yahoo.com>
2023-08-22 23:17:03 +0100

    connected to db

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,4 +1,5 @@
 import "express-async-errors"
+import mongoose from "mongoose";
 import cors from "cors"
 import express from "express"
 import dotenv from 'dotenv';
@@ -23,12 +24,12 @@ app.use(cors({
     credentials: true
 }));
 
-//connect to DB
-import connectDB from "./db/connect.js";
+
+
 
 //routes
 
-app.use('/api/v1/auth', authRouter)
+// app.use('/api/v1/auth', authRouter)
 
 
 app.use(notFoundMiddleware);
@@ -43,15 +44,29 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5500;
 
-const start = async() => {
+
+const connect = async() => {
     try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () =>
-            console.log(`Connect to DB || running on port ${port}...`)
-        );
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to DB...");
     } catch (error) {
         console.log(error);
     }
-};
+}
 
-start();
+app.listen(port, () => {
+    connect();
+})
+
+// const start = async() => {
+//     try {
+//         await connectDB(process.env.MONGO_URI)
+//         app.listen(port, () =>
+//             console.log("Connect to DB...")
+//         );
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+// start();
